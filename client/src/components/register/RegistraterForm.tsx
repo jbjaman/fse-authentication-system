b0c9b82815a6d5a1875e06a10d381495740e9323
jbjzeehad <ilamta.97@gmail.com>
2026-07-24 23:16:28 +0600

    feat: integrate react-hook-form for improved form handling in RegistraterForm component

--- client/src/components/register/RegistraterForm.tsx
+++ client/src/components/register/RegistraterForm.tsx
@@ -1,69 +1,57 @@
 "use client";
 
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const RegistraterForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({ name, email, password, confirmPassword });
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <div>
       <h1>Create Your Account</h1>
       <p>Welcome! Please create your account</p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="name">Name</label>
           <input
             id="name"
-            name="name"
             type="text"
             placeholder="Enter your name"
             autoComplete="name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+            {...register("name")}
           />
         </div>
         <div>
           <label htmlFor="email">Email</label>
           <input
             id="email"
-            name="email"
             type="email"
             placeholder="Enter your email"
             autoComplete="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
+            {...register("email")}
           />
         </div>
         <div>
           <label htmlFor="password">Password</label>
           <input
             id="password"
-            name="password"
             type="password"
             placeholder="Enter your password"
             autoComplete="new-password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
+            {...register("password")}
           />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input
             id="confirmPassword"
-            name="confirmPassword"
             type="password"
             placeholder="Enter your password"
             autoComplete="new-password"
-            value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)}
+            {...register("confirmPassword")}
           />
         </div>
         <button type="submit">Register</button>
