c3b06204ca1a201e7e32983a626b7a13327aaf91
jbjzeehad <ilamta.97@gmail.com>
2026-08-18 01:12:48 +0600

    feat: add registration schema and update register service and controller to validate input

--- server/src/controllers/auth/register.controller.ts
+++ server/src/controllers/auth/register.controller.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from "express";
+import { registerSchema } from "../../schemas/auth/register.schema.js";
 import { registerService } from "../../services/auth/register.service.js";
 
 export const registerController = async (req: Request, res: Response) => {
-  const result = await registerService();
+  const validatedData = registerSchema.parse(req.body);
+  const result = await registerService(validatedData);
   res.status(201).json({
     success: true,
     message: result.message,
--- server/src/services/auth/register.service.ts
+++ server/src/services/auth/register.service.ts
@@ -1,4 +1,10 @@
-export const registerService = async () => {
+export const registerService = async (data: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+  console.log(data);
+
   return {
     message: "Registration service is working",
   };
