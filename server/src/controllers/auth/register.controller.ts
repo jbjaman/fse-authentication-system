194360eeaf7a739a2a060434864fd58f37aabe3d
jbjzeehad <ilamta.97@gmail.com>
2026-08-17 23:54:13 +0600

    feat: implement register service and update register controller to use service response

--- server/src/controllers/auth/register.controller.ts
+++ server/src/controllers/auth/register.controller.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from "express";
+import { registerService } from "../../services/auth/register.service.js";
 
-export const registerController = (req: Request, res: Response) => {
+export const registerController = async (req: Request, res: Response) => {
+  const result = await registerService();
   res.status(201).json({
     success: true,
-    message: "Registrtion endpoint is working",
+    message: result.message,
   });
 };
---
+++ server/src/services/auth/register.service.ts
@@ -0,0 +1,5 @@
+export const registerService = async () => {
+  return {
+    message: "Registration service is working",
+  };
+};
